8d39ab5393ebe9981a182f3ddb973ed12ccaa141
Israel <israel_origin@outlook.com>
2022-01-16 12:44:58 -0300

    Find movies

--- backend/src/main/java/com/dev/dsmovie/dto/MovieDTO.java
+++ backend/src/main/java/com/dev/dsmovie/dto/MovieDTO.java
@@ -1,33 +1,24 @@
-package com.dev.dsmovie.entities;
+package com.dev.dsmovie.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.dev.dsmovie.entities.Movie;
 
-@Entity
-@Table(name = "tb_movie")
-public class Movie {
+public class MovieDTO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private Double score;
 	private Integer count;
 	private String image;
 
-	public Movie() {
-
+	public MovieDTO() {
 	}
 
-	public Movie(Long id, String title, Double score, Integer count, String image) {
-		this.id = id;
-		this.title = title;
-		this.score = score;
-		this.count = count;
-		this.image = image;
+	public MovieDTO(Movie movie) {
+		id = movie.getId();
+		title = movie.getTitle();
+		score = movie.getScore();
+		count = movie.getCount();
+		image = movie.getImage();
 	}
 
 	public Long getId() {
@@ -69,5 +60,4 @@ public class Movie {
 	public void setImage(String image) {
 		this.image = image;
 	}
-
 }
